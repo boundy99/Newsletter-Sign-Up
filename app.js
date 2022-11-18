08a5981f22e8208c863ea07ed12b871fc627f930
boundy99 <abdoulayeboundydjikine@gmail.com>
2022-11-17 23:07:18 -0500

    Phew

--- app.js
+++ app.js
@@ -1,23 +1,63 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
+const https = require('https')
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }))
-
 app.use(express.static("Public"))
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/HTML/signup.html")
 })
 
+
 app.post("/", (req, res) => {
     const firstName = req.body.firstName;
     const lastName = req.body.lastName;
     const email = req.body.email;
 
-    console.log(firstName, lastName, email);
+    var data = {
+        members: [{
+            email_address: email,
+            status: "subscribed",
+            merge_fields: {
+                FNAME: firstName,
+                LNAME: lastName
+            }
+        }]
+    }
+
+    const jsonData = JSON.stringify(data);
+    const url = "https://us21.api.mailchimp.com/3.0/lists/cdf1d20c2d"
+    const options = {
+        method: "POST",
+        auth: "boundy99:6d404b3640f3a76acca6c4b061088119-us2"
+    }
+    const request = https.request(url, options, (response) => {
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + "/HTML/sucess.html")
+        } else {
+            res.sendFile(__dirname + "/HTML/failure.html")
+        }
+        response.on("data", (data) => {
+            console.log(JSON.parse(data))
+        })
+    })
+    request.write(jsonData);
+
+    request.end();
 })
+app.post("/failure", (req, res) => {
+    res.redirect('/');
+})
+
 app.listen(3000, () => {
     console.log("Server is running on port 3000")
 })
+
+//API KEY    
+//6d404b3640f3a76acca6c4b061088119-us21
+
+//List ID
+//cdf1d20c2
